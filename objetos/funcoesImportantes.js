f43ca6fd5126f02283205bf9d242ac1a976aa24f
Nicholas Barbosa <47508677+nicholasfb@users.noreply.github.com>
2023-01-14 13:55:50 -0300

    Funções Importantes

---
+++ objetos/funcoesImportantes.js
@@ -0,0 +1,39 @@
+const pessoa = {
+    nome: 'Rebeca',
+    idade: 2,
+    peso: 13
+}
+
+const pessoa2 = {
+    nome: 'Junior',
+    idade: 22,
+    peso: 76
+}
+
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+console.log(Object.entries(pessoa));
+
+Object.entries(pessoa).forEach(elemento => {
+    console.log(`${elemento[0]}: ${elemento[1]}`)
+});
+
+Object.entries(pessoa2).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`);
+});
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true,
+    writable: false,
+    value: '01/01/2023'
+});
+
+pessoa.dataNascimento = '01/01/2019';
+console.log(pessoa.dataNascimento);
+console.log(Object.keys(pessoa));
+
+// Object.assign (ECMAScript 2015)
+const dest = { a: 1 }
+const o1 = { b: 2 }
+const o2 = { c: 3, a: 4 }
+const obj = Object.assign(dest, o1, o2);
